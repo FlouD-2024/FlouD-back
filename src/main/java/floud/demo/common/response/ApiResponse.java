17d509947cd1df08688b09ba464b43fe09d71003
kang <kang6@ewhain.net>
2024-02-29 23:45:20 +0900

    feat: add "success" info in api response

--- src/main/java/floud/demo/common/response/ApiResponse.java
+++ src/main/java/floud/demo/common/response/ApiResponse.java
@@ -13,12 +13,14 @@ import lombok.ToString;
 public class ApiResponse<T> {
 
     private final int code;
+    private final boolean success;
     private final String message;
     private final T data;
 
     public static <T> ApiResponse<T> success() {
         return new ApiResponse<>(
                 Success.SUCCESS.getHttpStatusCode(),
+                Success.SUCCESS.getHttpStatus().is2xxSuccessful(),
                 Success.SUCCESS.getMessage(),
                 null
         );
@@ -27,6 +29,7 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> success(Success success) {
         return new ApiResponse<>(
                 success.getHttpStatusCode(),
+                success.getHttpStatus().is2xxSuccessful(),
                 success.getMessage(),
                 null
         );
@@ -35,6 +38,7 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> success(Success success, T data) {
         return new ApiResponse<>(
                 success.getHttpStatusCode(),
+                success.getHttpStatus().is2xxSuccessful(),
                 success.getMessage(),
                 data
         );
@@ -43,6 +47,7 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> success(T data) {
         return new ApiResponse<>(
                 Success.SUCCESS.getHttpStatusCode(),
+                Success.SUCCESS.getHttpStatus().is2xxSuccessful(),
                 Success.SUCCESS.getMessage(),
                 data
         );
@@ -52,6 +57,7 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> failure(Error error) {
         return new ApiResponse<>(
                 error.getHttpStatusCode(),
+                error.getHttpStatus().is2xxSuccessful(),
                 error.getMessage(),
                 null
         );
@@ -60,6 +66,7 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> failure(Error error, String message) {
         return new ApiResponse<>(
                 error.getHttpStatusCode(),
+                error.getHttpStatus().is2xxSuccessful(),
                 message,
                 null
         );
@@ -68,6 +75,7 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> failure(Error error, T data) {
         return new ApiResponse<>(
                 error.getHttpStatusCode(),
+                error.getHttpStatus().is2xxSuccessful(),
                 error.getMessage(),
                 data
         );
@@ -76,6 +84,7 @@ public class ApiResponse<T> {
     public static <T> ApiResponse<T> failure(T data) {
         return new ApiResponse<>(
                 Error.ERROR.getHttpStatusCode(),
+                Error.ERROR.getHttpStatus().is2xxSuccessful(),
                 Error.ERROR.getMessage(),
                 data
         );
