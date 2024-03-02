8a4a031de8eaa4f3b819d854bdb1e948246272db
kang <kang6@ewhain.net>
2024-03-03 02:52:21 +0900

    chore: add get memoir success msg

--- src/main/java/floud/demo/common/response/Success.java
+++ src/main/java/floud/demo/common/response/Success.java
@@ -13,7 +13,8 @@ public enum Success {
 
     //200 SUCCESS
     MEMOIR_UPDATE_SUCCESS(HttpStatus.OK , "성공적으로 회고를 수정하였습니다."),
-    ONE_MEMOIR_GET_SUCCESS(HttpStatus.OK , "하나의 회고를 성공적으로 조회하였습니다."),
+    GET_MY_MEMOIR_SUCCESS(HttpStatus.OK , "나의 회고를 성공적으로 조회하였습니다."),
+    GET_FRIEND_MEMOIR_SUCCESS(HttpStatus.OK , "친구의 회고를 성공적으로 조회하였습니다."),
     MULTIPLE_MEMOIR_GET_SUCCESS(HttpStatus.OK , "주차별 회고 목록을 성공적으로 조회하였습니다."),
 
     //201 CREATED SUCCESS
