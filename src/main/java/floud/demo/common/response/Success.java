0ff4744cdb4f50c53ad02c0a04c4b0d6b369dd9c
kang <kang6@ewhain.net>
2024-03-03 01:58:01 +0900

    chore: change msg for get multiple memoir

--- src/main/java/floud/demo/common/response/Success.java
+++ src/main/java/floud/demo/common/response/Success.java
@@ -14,7 +14,7 @@ public enum Success {
     //200 SUCCESS
     MEMOIR_UPDATE_SUCCESS(HttpStatus.OK , "성공적으로 회고를 수정하였습니다."),
     ONE_MEMOIR_GET_SUCCESS(HttpStatus.OK , "하나의 회고를 성공적으로 조회하였습니다."),
-    MULTIPLE_MEMOIR_GET_SUCCESS(HttpStatus.OK , "이번 주 회고 목록을 성공적으로 조회하였습니다."),
+    MULTIPLE_MEMOIR_GET_SUCCESS(HttpStatus.OK , "주차별 회고 목록을 성공적으로 조회하였습니다."),
 
     //201 CREATED SUCCESS
     MEMOIR_CREATE_SUCCESS(HttpStatus.CREATED, "성공적으로 회고를 등록하였습니다."),
