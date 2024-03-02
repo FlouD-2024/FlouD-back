baa66af919c8a341f1c351e0e43f1d5b69e2b31f
kang <kang6@ewhain.net>
2024-03-02 19:47:42 +0900

    feat: create memoir's controller, repository and service basic structure

---
+++ src/main/java/floud/demo/controller/MemoirController.java
@@ -0,0 +1,11 @@
+package floud.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/memoir")
+public class MemoirController {
+}
