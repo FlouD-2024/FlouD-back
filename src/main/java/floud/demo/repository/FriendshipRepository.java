41974a021a080773f3b25258af6fd7b49103fdb9
kang <kang6@ewhain.net>
2024-03-04 01:42:51 +0900

    feat: add comment and findByUserIds query

--- src/main/java/floud/demo/repository/FriendshipRepository.java
+++ src/main/java/floud/demo/repository/FriendshipRepository.java
@@ -8,19 +8,26 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+    //나랑 친구인 유저 목록
     @Query(value = "SELECT * FROM friendship f " +
             "WHERE f.friendship_status = 'ACCEPT' " +
             "AND (f.to_user = :users_id OR f.from_user = :users_id)", nativeQuery = true)
     List<Friendship> findAllByUsersId(Long users_id);
 
+    //현재 친구 관계가 있는 지 확인
     @Query(value = "SELECT * FROM Friendship f " +
             "WHERE (f.to_user = :toUserId AND f.from_user = :fromUserId) " +
             "OR (f.to_user = :fromUserId AND f.from_user = :toUserId)",  nativeQuery = true)
-    Optional<Friendship> findByToUserAndFromUser(Long toUserId, Long fromUserId);
+    Optional<Friendship> checkExistingFriendship(Long toUserId, Long fromUserId);
 
+    //대기 상태인 친구들
     @Query(value = "SELECT * FROM friendship f " +
-            "WHERE f.friendship_status = 'WAITING' AND  f.from_user = :users_id", nativeQuery = true)
+            "WHERE f.friendship_status = 'WAITING' AND  f.to_user = :users_id", nativeQuery = true)
     List<Friendship> findAllByWaitingToUser(Long users_id);
 
+    //수락하기 위해 대기 상태인 Friendship 찾기
+    @Query(value = "SELECT * FROM friendship f " +
+            "WHERE f.friendship_status = 'WAITING' AND  f.to_user = :to_user AND f.from_user = :from_user", nativeQuery = true)
+    Friendship findByUserIds(Long to_user, Long from_user);
 
 }
