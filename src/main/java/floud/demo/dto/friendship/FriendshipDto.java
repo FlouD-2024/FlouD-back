0efab368e01ac2239d19074bd92554035998b925
kang <kang6@ewhain.net>
2024-03-03 16:35:49 +0900

    feat: create response dto for get friendship list

---
+++ src/main/java/floud/demo/dto/friendship/FriendshipDto.java
@@ -0,0 +1,8 @@
+package floud.demo.dto.friendship;
+
+import java.util.List;
+
+public class FriendshipDto {
+    private String nickname;
+    private List<FriendshipDto> friendshipList;
+}
---
+++ src/main/java/floud/demo/dto/friendship/FriendshipListResponseDto.java
@@ -0,0 +1,10 @@
+package floud.demo.dto.friendship;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class FriendshipListResponseDto {
+
+}
