dc34a671b906b74fa1176d812dfbdb45e2ea7e6b
kang <kang6@ewhain.net>
2024-03-04 02:23:05 +0900

    chore: add success msg  for delete friend api

--- src/main/java/floud/demo/common/response/Success.java
+++ src/main/java/floud/demo/common/response/Success.java
@@ -23,6 +23,7 @@ public enum Success {
 
     GET_FRIEND_LIST_SUCCESS(HttpStatus.OK , "나의 친구 목록을 성공적으로 조회하였습니다."),
     GET_FRIEND_MEMOIR_SUCCESS(HttpStatus.OK , "친구의 회고를 성공적으로 조회하였습니다."),
+    DELETE_FRIEND_SUCCESS(HttpStatus.OK , "친구를 성공적으로 삭제하였습니다."),
 
     //201 CREATED SUCCESS
     CREATE_MEMOIR_SUCCESS(HttpStatus.CREATED, "성공적으로 회고를 등록하였습니다."),
