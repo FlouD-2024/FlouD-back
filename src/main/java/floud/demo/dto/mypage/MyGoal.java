f078d0e31a1cf33977bd84bb0724e507d4d5e6d8
kang <kang6@ewhain.net>
2024-03-03 14:04:54 +0900

    chore: add builder

--- src/main/java/floud/demo/dto/mypage/MyGoal.java
+++ src/main/java/floud/demo/dto/mypage/MyGoal.java
@@ -1,10 +1,12 @@
 package floud.demo.dto.mypage;
 
 
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Getter
 public class MyGoal {
     private Long goal_id;
