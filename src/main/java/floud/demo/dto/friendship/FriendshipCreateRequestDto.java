1f57006f91c5fccad5e7ab5420e7181ae04d9c53
kang <kang6@ewhain.net>
2024-03-03 17:53:47 +0900

    feat: toEntity method

--- src/main/java/floud/demo/dto/friendship/FriendshipCreateRequestDto.java
+++ src/main/java/floud/demo/dto/friendship/FriendshipCreateRequestDto.java
@@ -1,8 +1,19 @@
 package floud.demo.dto.friendship;
 
+import floud.demo.domain.Friendship;
+import floud.demo.domain.Users;
+import floud.demo.domain.enums.FriendshipStatus;
 import lombok.Getter;
 
 @Getter
 public class FriendshipCreateRequestDto {
     private String nickname;
+
+    public Friendship toEntity(Users to_user, Users from_user){
+        return Friendship.builder()
+                .to_user(to_user)
+                .from_user(from_user)
+                .friendshipStatus(FriendshipStatus.WAITING)
+                .build();
+    }
 }
