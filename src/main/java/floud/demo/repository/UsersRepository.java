ae3afee208616ea4d07f21d62e9d175e9fcb939c
kang <kang6@ewhain.net>
2024-03-03 17:53:36 +0900

    feat: findByNickname

--- src/main/java/floud/demo/repository/UsersRepository.java
+++ src/main/java/floud/demo/repository/UsersRepository.java
@@ -2,8 +2,11 @@ package floud.demo.repository;
 
 import floud.demo.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
     boolean existsByNickname(String nickname);
+
+    Optional<Users> findByNickname(String nickname);
 }
