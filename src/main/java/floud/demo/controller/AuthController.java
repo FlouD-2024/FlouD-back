922dd61dbe7e7554eb7cbd7ec014ae1723b2b114
Seongjoon-K <jobcho6320@naver.com>
2024-03-04 18:33:01 +0900

    feat: create authController google login callback

--- src/main/java/floud/demo/controller/AuthController.java
+++ src/main/java/floud/demo/controller/AuthController.java
@@ -1,12 +1,24 @@
 package floud.demo.controller;
 
+import floud.demo.service.AuthService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    @GetMapping("/callback/google")
+    public ResponseEntity<String> successGoogleLogin(@RequestParam("code") String code) {
+        return authService.getGoogleAccessToken(code);
+    }
 
 }
