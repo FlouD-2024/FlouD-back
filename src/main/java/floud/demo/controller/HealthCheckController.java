0e4dc843a46da956dfe7347a194b87ba4a0298c8
Seongjoon-K <jobcho6320@naver.com>
2024-03-05 02:16:44 +0900

    feat: create healthcheck controller for load balancer

---
+++ src/main/java/floud/demo/controller/HealthCheckController.java
@@ -0,0 +1,18 @@
+package floud.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String healthCheck() {
+        return "welcome floud";
+    }
+}
