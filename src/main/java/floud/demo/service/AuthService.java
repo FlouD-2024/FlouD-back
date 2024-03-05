fbd3ea5dce6a53da7871bc7b867987bc41c30c4b
Seongjoon-K <jobcho6320@naver.com>
2024-03-05 17:51:32 +0900

    feat: create to convert token payload info

--- src/main/java/floud/demo/service/AuthService.java
+++ src/main/java/floud/demo/service/AuthService.java
@@ -1,10 +1,20 @@
 package floud.demo.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import floud.demo.common.response.ApiResponse;
 import floud.demo.common.response.Success;
+import floud.demo.domain.Users;
 import floud.demo.dto.auth.TokenResponseDto;
+import floud.demo.dto.auth.UsersResponseDto;
+import floud.demo.repository.UsersRepository;
+import io.jsonwebtoken.io.Decoders;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -13,13 +23,17 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
+    @Autowired
+    private final UsersRepository usersRepository;
     private final String GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token";
     private final String KAKAO_TOKEN_URL = "https://kauth.kakao.com/oauth/token";
 
@@ -71,11 +85,11 @@ public class AuthService {
                 ObjectMapper objectMapper = new ObjectMapper();
                 TokenResponseDto tokenResponseDto = objectMapper.readValue(responseEntity.getBody(), TokenResponseDto.class);
 
-                String accessToken = tokenResponseDto.getAccess_token();
+                String idToken = tokenResponseDto.getId_token();
                 String refreshToken = tokenResponseDto.getRefresh_token();
 
                 return ApiResponse.success(Success.GET_GOOGLE_ACCESS_TOKEN_SUCCESS, TokenResponseDto.builder()
-                        .access_token(accessToken)
+                        .id_token(idToken)
                         .refresh_token(refreshToken)
                         .build());
             } else {
@@ -105,7 +119,7 @@ public class AuthService {
         MultiValueMap<String, String> params= new LinkedMultiValueMap<>();
         params.add("code", code);
         params.add("client_id", KAKAO_CLIENT_ID);
-         params.add("client_secret", KAKAO_CLIENT_SECRET);
+        params.add("client_secret", KAKAO_CLIENT_SECRET);
         params.add("redirect_uri", KAKAO_REDIRECT_URI);
         params.add("grant_type", "authorization_code");
 
@@ -119,11 +133,11 @@ public class AuthService {
                 ObjectMapper objectMapper = new ObjectMapper();
                 TokenResponseDto tokenResponseDto = objectMapper.readValue(responseEntity.getBody(), TokenResponseDto.class);
 
-                String accessToken = tokenResponseDto.getAccess_token();
+                String id_token = tokenResponseDto.getId_token();
                 String refreshToken = tokenResponseDto.getRefresh_token();
 
                 return ApiResponse.success(Success.GET_KAKAO_ACCESS_TOKEN_SUCCESS, TokenResponseDto.builder()
-                        .access_token(accessToken)
+                        .id_token(id_token)
                         .refresh_token(refreshToken)
                         .build());
             } else {
@@ -133,5 +147,77 @@ public class AuthService {
             e.printStackTrace();
             return ApiResponse.failure(e);
         }
+    }
+
+    public ApiResponse<?> getUserInfoByToken(String authorizationHeader) {
+
+        System.out.println("authorizationHeader = " + authorizationHeader);
+
+        String token = authorizationHeader.substring("Bearer ".length());
+        UsersResponseDto userinfo = decodeToken(token);
+        Users getUser = findUserBySocial_id(userinfo.getSocial_id());
+        System.out.println("getUser = " + getUser);
+        UsersResponseDto usersInfoResponse = userinfo.builder()
+                .users_id(getUser.getId())
+                .email(getUser.getEmail())
+                .social_id(getUser.getSocial_id())
+                .build();
+
+        return ApiResponse.success(Success.GET_USER_INFO_SUCCESS, usersInfoResponse);
+    }
+
+    public UsersResponseDto decodeToken(String token) {
+        String decode = decryptBase64UrlToken(token.split("\\.")[1]);
+        System.out.println("decode = " + decode);
+        return transJsonToMemberInfoDto(decode);
+    }
+
+    public String decryptBase64UrlToken(String jwtToken) {
+        byte[] decode = Decoders.BASE64URL.decode(jwtToken);
+        return new String(decode, StandardCharsets.UTF_8);
+    }
+
+    public UsersResponseDto transJsonToMemberInfoDto(String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            UsersResponseDto dto = mapper.readValue(json, UsersResponseDto.class);
+            System.out.println("UsersRespone로 변경= " + dto);
+            saveMemberInfo(dto);
+            return dto;
+        } catch (JsonMappingException e) {
+            throw new RuntimeException(e);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional
+    public void saveMemberInfo(UsersResponseDto dto) {
+        try {
+            Optional<Users> user = usersRepository.findBySocial_id(dto.getSocial_id());
+            System.out.println("멤버 조회");
+            System.out.println("OAuth ID: " + dto.getSocial_id());
+            System.out.println("member 존재 여부: " + user.isPresent());
+            processMember(user, dto);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void processMember(Optional<Users> users, UsersResponseDto dto) {
+        users.ifPresentOrElse(
+                existingUser -> {
+                    System.out.println("유저가 이미 존재합니다: " + existingUser);
+                },
+                () -> {
+                    Users newUser = dto.toEntity();
+                    System.out.println("새 멤버 저장됨: " + newUser);
+                    usersRepository.save(newUser);
+                }
+        );
+    }
+    public Users findUserBySocial_id(String social_id) {
+        return usersRepository.findBySocial_id(social_id)
+                .orElseThrow(() -> new EntityNotFoundException("Member not found"));
     }
 }
