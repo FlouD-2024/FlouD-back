23d6e7ae6a93797e99f71ed37b86ccaa2139b689
Seongjoon-K <jobcho6320@naver.com>
2024-03-05 17:48:34 +0900

    feat: create find by social id for search user

--- src/main/java/floud/demo/repository/UsersRepository.java
+++ src/main/java/floud/demo/repository/UsersRepository.java
@@ -2,6 +2,7 @@ package floud.demo.repository;
 
 import floud.demo.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,4 +10,7 @@ public interface UsersRepository extends JpaRepository<Users, Long> {
     boolean existsByNickname(String nickname);
 
     Optional<Users> findByNickname(String nickname);
+
+    @Query("SELECT m FROM Users m WHERE (:social_id IS NULL OR m.social_id = :social_id)")
+    Optional<Users> findBySocial_id(String social_id);
 }
