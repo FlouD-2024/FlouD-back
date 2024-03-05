de7ad82a8ef814f3bd885be15c74b7f4309652d5
Seongjoon-K <jobcho6320@naver.com>
2024-03-05 17:52:50 +0900

    feat: create get user info api

--- src/main/java/floud/demo/controller/AuthController.java
+++ src/main/java/floud/demo/controller/AuthController.java
@@ -5,10 +5,7 @@ import floud.demo.service.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
@@ -36,5 +33,10 @@ public class AuthController {
     @GetMapping("/kakao/login")
     public RedirectView redirectToKakao() {
         return authService.redirectToKakao();
+    }
+
+    @GetMapping("/decode")
+    public ApiResponse<?> getUserInfoByToken(@RequestHeader(value="Authorization") String authorizationHeader) {
+        return authService.getUserInfoByToken(authorizationHeader);
     }
 }
