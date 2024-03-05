1a194172057e6b483fa4e0716ca67312192523e0
Yeonsoo Kang <90603399+Kang1221@users.noreply.github.com>
2024-03-05 23:42:07 +0900

    Revert "Feat: Friend 관련 api 추가"

--- src/main/java/floud/demo/dto/friendship/FriendshipListResponseDto.java
+++ src/main/java/floud/demo/dto/friendship/FriendshipListResponseDto.java
@@ -9,7 +9,5 @@ import java.util.List;
 @Builder
 public class FriendshipListResponseDto {
     private String my_nickname;
-    private Integer totalFriendNum;
-    private Integer totalPage;
     private List<FriendshipDto> friendshipList;
 }
