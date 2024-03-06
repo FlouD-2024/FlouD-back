a945bd2dc04235a86c33bca557bb72238dd79f68
kang <kang6@ewhain.net>
2024-03-07 00:46:17 +0900

    feat: create notification entity

---
+++ src/main/java/floud/demo/domain/Notification.java
@@ -0,0 +1,31 @@
+package floud.demo.domain;
+
+import floud.demo.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Notification extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "users_id")
+    private Users users;
+
+    @Column(nullable = false)
+    private String message;
+
+    @Builder
+    public Notification(Long id, Users users, String message){
+        this.id = id;
+        this.users = users;
+        this.message = message;
+    }
+}
