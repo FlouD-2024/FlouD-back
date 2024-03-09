8711ecd7f839a36e36fbe69fca2fd4318f2fa58b
kang <kang6@ewhain.net>
2024-03-09 21:51:06 +0900

    fix: change column name

--- src/main/java/floud/demo/repository/CommunityRepository.java
+++ src/main/java/floud/demo/repository/CommunityRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CommunityRepository extends JpaRepository<Community, Long> {
-    @Query(value = "SELECT * FROM Community c WHERE c.postType = :postType ORDER BY c.created_at DESC limit 30", nativeQuery = true)
+    @Query(value = "SELECT * FROM Community c WHERE c.post_type = :postType ORDER BY c.created_at DESC limit 30", nativeQuery = true)
     List<Community> findTop30ByPostType(String postType);
 }
