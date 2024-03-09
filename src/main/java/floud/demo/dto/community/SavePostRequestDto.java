1f783f1318f80c62990320bdd41d811bf9f524d0
kang <kang6@ewhain.net>
2024-03-09 21:31:21 +0900

    feat: add toEntity method

--- src/main/java/floud/demo/dto/community/SavePostRequestDto.java
+++ src/main/java/floud/demo/dto/community/SavePostRequestDto.java
@@ -1,9 +1,19 @@
 package floud.demo.dto.community;
 
+import floud.demo.domain.Community;
+import floud.demo.domain.Users;
 import lombok.Getter;
 
 @Getter
 public class SavePostRequestDto {
     private String title;
     private String content;
+
+    public Community toEntity(Users user){
+        return Community.builder()
+                .title(title)
+                .content(content)
+                .users(user)
+                .build();
+    }
 }
