8b77b796c36cc6fcd92d210c355c4305e67f3916
Seongjoon-K <jobcho6320@naver.com>
2024-03-10 02:30:47 +0900

    refactor: modify health check endpoint

--- src/main/java/floud/demo/controller/HealthCheckController.java
+++ src/main/java/floud/demo/controller/HealthCheckController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/health")
+@RequestMapping("/")
 public class HealthCheckController {
 
     @GetMapping
