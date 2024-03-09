945f83aa47b90ae4502182ca374b047cd109d579
kang <kang6@ewhain.net>
2024-03-09 20:31:36 +0900

    feat: create get community

--- src/main/java/floud/demo/service/CommunityService.java
+++ src/main/java/floud/demo/service/CommunityService.java
@@ -5,11 +5,14 @@ import floud.demo.common.response.Success;
 import floud.demo.domain.Community;
 import floud.demo.domain.Users;
 import floud.demo.domain.enums.PostType;
+import floud.demo.dto.community.CommunityResponseDto;
+import floud.demo.dto.community.Post;
 import floud.demo.repository.CommunityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -20,9 +23,20 @@ public class CommunityService {
         //Get user
         Users users = authService.findUserByToken(authorizationHeader);
 
-        List<Community> communityList =  communityRepository.findAll();
+        List<Community> communityList =  communityRepository.findTop30ByPostType(postType.toString());
+        List<Post> postList = communityList.stream().
+                map(community -> Post.builder()
+                        .community_id(community.getId())
+                        .title(community.getTitle())
+                        .content(community.getContent())
+                        .postType(community.getPostType())
+                        .build())
+                .collect(Collectors.toList());
 
-
-        return ApiResponse.success(Success.GET_COMMUNITY_FIND_FRIEND_SUCCESS);
+        return ApiResponse.success(Success.GET_COMMUNITY_FIND_FRIEND_SUCCESS, CommunityResponseDto.builder()
+                .nickname(users.getNickname())
+                .postType(postType)
+                .postList(postList)
+                .build());
     }
 }
