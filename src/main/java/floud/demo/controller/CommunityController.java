698d08bcae83af05fc279f7d78cd13d372e1c8c6
kang <kang6@ewhain.net>
2024-03-09 20:06:34 +0900

    feat: basic structure of get community

--- src/main/java/floud/demo/controller/CommunityController.java
+++ src/main/java/floud/demo/controller/CommunityController.java
@@ -1,11 +1,20 @@
 package floud.demo.controller;
 
+import floud.demo.common.response.ApiResponse;
+import floud.demo.domain.enums.PostType;
+import floud.demo.service.CommunityService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/community")
 public class CommunityController {
+    private final CommunityService communityService;
+
+    @GetMapping
+    public ApiResponse<?> getCommunity(@RequestHeader(value="Authorization") String authorizationHeader,
+                                       @RequestParam(name = "post_type")PostType postType){
+        return communityService.getCommunity(authorizationHeader, postType);
+    }
 }
--- src/main/java/floud/demo/service/CommunityService.java
+++ src/main/java/floud/demo/service/CommunityService.java
@@ -1,9 +1,28 @@
 package floud.demo.service;
 
+import floud.demo.common.response.ApiResponse;
+import floud.demo.common.response.Success;
+import floud.demo.domain.Community;
+import floud.demo.domain.Users;
+import floud.demo.domain.enums.PostType;
+import floud.demo.repository.CommunityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CommunityService {
+    private final AuthService authService;
+    private final CommunityRepository communityRepository;
+    public ApiResponse<?> getCommunity(String authorizationHeader, PostType postType){
+        //Get user
+        Users users = authService.findUserByToken(authorizationHeader);
+
+        List<Community> communityList =  communityRepository.findAll();
+
+
+        return ApiResponse.success(Success.GET_COMMUNITY_FIND_FRIEND_SUCCESS);
+    }
 }
