1444ef7a7d35e511c0a104bf5907b7829e66a7ea
kang <kang6@ewhain.net>
2024-03-09 21:05:11 +0900

    chore: change abstract class to class

--- src/main/java/floud/demo/common/exception/NotFoundException.java
+++ src/main/java/floud/demo/common/exception/NotFoundException.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public abstract class NotFoundException extends ApiException {
+public class NotFoundException extends ApiException {
     public NotFoundException(final Error message) {
         super(NOT_FOUND, message, null);
     }
