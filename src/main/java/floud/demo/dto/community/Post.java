42d228ec4e6b270b6bbeee955b073db79d62faea
kang <kang6@ewhain.net>
2024-03-09 20:31:22 +0900

    chore: add getter builder

--- src/main/java/floud/demo/dto/community/Post.java
+++ src/main/java/floud/demo/dto/community/Post.java
@@ -1,7 +1,14 @@
 package floud.demo.dto.community;
 
+import floud.demo.domain.enums.PostType;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
 public class Post {
     private Long community_id;
     private String title;
     private String content;
+    private PostType postType;
 }
