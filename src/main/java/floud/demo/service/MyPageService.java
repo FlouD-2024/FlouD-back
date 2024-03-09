5ae15225949dd42488ebb505909ea440b00644c3
kang <kang6@ewhain.net>
2024-03-09 18:23:00 +0900

    feat: add AlarmStatus field when create friendship

--- src/main/java/floud/demo/service/MyPageService.java
+++ src/main/java/floud/demo/service/MyPageService.java
@@ -7,6 +7,7 @@ import floud.demo.domain.Alarm;
 import floud.demo.domain.Friendship;
 import floud.demo.domain.Goal;
 import floud.demo.domain.Users;
+import floud.demo.domain.enums.AlarmStatus;
 import floud.demo.domain.enums.FriendshipStatus;
 import floud.demo.dto.mypage.*;
 import floud.demo.dto.mypage.dto.MyFriend;
@@ -239,7 +240,7 @@ public class MyPageService {
 
     private void createAlarm(Users from_user, Users to_user){
         String message = "친구 신청이 수락되었습니다.";
-        alarmRepository.save(new Alarm(from_user, to_user.getNickname(), message));
+        alarmRepository.save(new Alarm(from_user, to_user.getNickname(), AlarmStatus.FRIEND, message));
     }
 
 
