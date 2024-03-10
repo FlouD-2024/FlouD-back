88688f2447b68258bcf8dba0d77e81182e2a2c80
Seongjoon-K <jobcho6320@naver.com>
2024-03-10 17:27:13 +0900

    chore: modify health check endpoint

--- src/main/java/floud/demo/controller/HealthCheckController.java
+++ src/main/java/floud/demo/controller/HealthCheckController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/health")
 public class HealthCheckController {
 
     @GetMapping
