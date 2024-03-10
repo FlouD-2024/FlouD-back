288e465efd1c0eb1dc0af2e67b7d610f4bb8e00f
kang <kang6@ewhain.net>
2024-03-10 18:17:16 +0900

    feat: add page Info dto

---
+++ src/main/java/floud/demo/dto/PageInfo.java
@@ -0,0 +1,13 @@
+package floud.demo.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PageInfo {
+    private Integer nowPage;
+    private Integer totalPages;
+    private Long totalElements;
+    private Boolean last;
+}
