234e8b3b7bd2f2e84f21c98d8c2547e68cd4a488
kang <kang6@ewhain.net>
2024-03-10 19:22:48 +0900

    refact: move file

--- src/main/java/floud/demo/config/WebMvcConfiguration.java
+++ src/main/java/floud/demo/config/WebMvcConfiguration.java
@@ -1,4 +1,4 @@
-package floud.demo.common.config;
+package floud.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
