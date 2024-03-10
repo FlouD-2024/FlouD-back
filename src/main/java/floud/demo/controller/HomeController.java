6e1e30a698f840ac113b4cbd9723c33c5f99613e
kang <kang6@ewhain.net>
2024-03-10 19:58:46 +0900

    feat: create Home controller, service

---
+++ src/main/java/floud/demo/controller/HomeController.java
@@ -0,0 +1,9 @@
+package floud.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class HomeController {
+}
