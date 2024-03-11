3853507f77326e0aea4ae9c6f22a07079b8417f6
kang <kang6@ewhain.net>
2024-03-11 21:35:31 +0900

    feat: basic structure of get home api

--- src/main/java/floud/demo/controller/HomeController.java
+++ src/main/java/floud/demo/controller/HomeController.java
@@ -1,10 +1,20 @@
 package floud.demo.controller;
 
+import floud.demo.common.response.ApiResponse;
+import floud.demo.service.HomeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("api/home")
 public class HomeController {
-
+    private final HomeService homeService;
+    @GetMapping
+    public ApiResponse<?> getHome(@RequestHeader(value="Authorization") String authorizationHeader){
+        return homeService.getHome(authorizationHeader);
+    }
 }
--- src/main/java/floud/demo/service/HomeService.java
+++ src/main/java/floud/demo/service/HomeService.java
@@ -1,9 +1,21 @@
 package floud.demo.service;
 
+import floud.demo.common.response.ApiResponse;
+import floud.demo.common.response.Success;
+import floud.demo.domain.Users;
+import floud.demo.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class HomeService {
+    private final AuthService authService;
+    private final UsersRepository usersRepository;
+
+    public ApiResponse<?> getHome(String authorizationHeader) {
+        //Get user
+        Users users = authService.findUserByToken(authorizationHeader);
+        return ApiResponse.success(Success.GET_HOME_SUCCESS);
+    }
 }
